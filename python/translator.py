46318f9c9f8dad759da2685120e9894d3530a0c1
Ivy Xu <140064242+ivymxu@users.noreply.github.com>
2024-09-03 05:10:11 +0000

    determining english or braille

--- python/translator.py
+++ python/translator.py
@@ -1,1 +1,8 @@
 # determine if the given string should be translated to English or Braille
+
+def determine_language(text):
+    # if the text contains any non-ascii characters, it is assumed to be in English
+    if any(ord(char) > 127 for char in text):
+        return 'english'
+    else:
+        return 'braille'
